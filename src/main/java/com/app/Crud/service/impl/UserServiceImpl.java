64f7a081241596003b109bdf0c7e5f85531a67c9
amritkh078 <amritkhatiwada078@gmail.com>
2024-02-05 13:32:08 +0545

    service implementation

---
+++ src/main/java/com/app/Crud/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.app.Crud.service.impl;
+
+import com.app.Crud.entity.UserEntity;
+import com.app.Crud.repository.UserRepository;
+import com.app.Crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void addUser(UserEntity userEntity) {
+        userRepository.save(userEntity);
+    }
+
+    @Override
+    public List<UserEntity> getUsers() {
+        return userRepository.findAll();
+    }
+}
